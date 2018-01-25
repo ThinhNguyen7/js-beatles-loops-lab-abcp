05822083341879fda7584096385155444b5f7034
THINH NGUYEN <thinhnguyen74@gmail.com>
2018-01-25 00:13:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
+const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
+function theBeatlesPlay(musicians, instruments){
+  var play = []
+  for (i=0; i < 4; i++){
+    console.log(musicians$[i] `plays` instruments$[i])
+  } return play
+}
