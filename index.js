921881c60a806aee323487ecbf30d1e6a34bd616
THINH NGUYEN <thinhnguyen74@gmail.com>
2018-01-25 00:23:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 function theBeatlesPlay(musicians, instruments){
   var play = []
   for (let i=0; i < 4; i++){
-    play.push(`$)
+    play.push(`${musicians[i] play $instruments[i]`)
     
     
   }
